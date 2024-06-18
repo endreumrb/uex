bdecece1458926e409d67b116dd11516e586d312
Endreu Benites <endreu.matteusrb@gmail.com@gmail.com>
2024-06-18 20:44:49 -0300

    Ajuste e implementação das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,28 @@
 <?php
 
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+    Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+
+    Route::get('/enderecos', [AddressController::class, 'search'])->name('enderecos.buscar');
 });
 
-require __DIR__.'/auth.php';
+
+
+require __DIR__ . '/auth.php';
